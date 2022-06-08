b7129ff3a916b8f47bb782464296dcba4e898767
AVacAdok <avakuang8@gmail.com>
2022-06-08 22:07:28 +0000

    export connect from cx.js/ import connect on play.js

---
+++ client.js
@@ -0,0 +1,31 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function() {
+  
+  console.log("Connecting ...");
+  const conn = net.createConnection({
+    host: "165.227.47.243", // IP address here,
+    port: 50541, // PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  
+  //print out data from server when recevies
+  conn.on("data", (data) => {
+    console.log("Server says: ", data);
+  });
+  //send msg to the server when connects
+  conn.on("connect", () => {
+    conn.write("Hello from client!");
+  });
+
+  return conn;
+};
+
+
+module.exports = {
+  connect
+
+};
--- play.js
+++ play.js
@@ -1,27 +1,6 @@
-const net = require("net");
+//we need to destructure the import same way of the export
+const {connect} = require("./client");
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "165.227.47.243", // IP address here,
-    port: 50541, // PORT number here,
-  });
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  
-  //print out data from server when recevies
-  conn.on("data", (data) => {
-  console.log("Server says: ", data);
-  });
-  //send msg to the server when connects
-  conn.on("connect", () => {
-  conn.write("Hello from client!");
-  });
-
-  return conn;
-};
-
-console.log("Connecting ...");
 connect();
 
