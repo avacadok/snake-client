e4968bcf851b13050d7a86b5ae94571e55dc263d
AVacAdok <avakuang8@gmail.com>
2022-06-09 02:25:27 +0000

    turn hard-coded data into constants & added comments

--- play.js
+++ play.js
@@ -6,3 +6,13 @@ const {setupInput} = require("./input");
 console.log("Connecting ...\n");
 
 setupInput(connect());
+
+/*Perhaps the player's name/initials. That said, one could argue that this should be provided as a command line argument instead
+The movement keys (W, A, S, D) could be added to constants like like MOVE_UP_KEY, etc.
+What about the key mappings for messages?
+Every time a user presses the key 'x', it displays the message 'y'
+This seems like a mapping that's perfect for an object ({ x: "y" })
+Instead of using if statements to implement this, you can define an object (as a constant) which
+defines these message mappings. Your logic would change to simply lookup the appropriate mapping in the object, and if found, send
+the value as the message string
+*/
