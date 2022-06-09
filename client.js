249bfb11f67411e5705d74507bfe6076068523b9
AVacAdok <avakuang8@gmail.com>
2022-06-09 02:16:02 +0000

    turn hard-coded data into constants

--- client.js
+++ client.js
@@ -1,11 +1,15 @@
 const net = require("net");
+const { IP, Port } = require("./constants");
+
 
 // establishes a connection with the game server
 const connect = function() {
   
   const conn = net.createConnection({
-    host: "165.227.47.243", // IP address here,
-    port: 50541, // PORT number here,
+    host: IP,
+    port: Port,
+    // host: "165.227.47.243", // IP address here,
+    // port: 50541, // PORT number here,
   });
 
   // interpret incoming data as text
